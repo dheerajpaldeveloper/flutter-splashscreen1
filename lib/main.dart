d4c848b9160e9d14b6cf2d6c83f27283bcd33818
root <root@localhost.localdomain>
2019-09-14 17:01:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MaterialApp(
+theme: ThemeData(primaryColor: Colors.red,accentColor: Colors.yellowAccent),
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+)
+);
+
+class SplashScreen extends StatefulWidget{
+    @override
+  _SplashScreenState createState()=> _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5),()=> print("splash done"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.orangeAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 100.0,
+                      child: Icon(
+                        Icons.android,
+                        color: Colors.orangeAccent,
+                        size: 100.0,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 40.0),
+                    ),
+                    Text(
+                      "Flutter",
+                      style: TextStyle(color: Colors.white,
+                      fontSize: 30.0,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text("Splash Screen",
+                        style: TextStyle(color: Colors.white,
+                        fontSize: 30.0,
+                        ),
+                    )
+                  ],
+                  ),
+              ),
+              ),
+              Expanded(flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                SizedBox(
+
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.tealAccent),
+                      strokeWidth: 5.0,
+                    //backgroundColor: Colors.redAccent,
+                  ),
+                  height: 70.0,
+                  width: 70.0,
+                ),
+                  Padding(padding: EdgeInsets.only(top: 40.0),
+
+                  ),
+                  Text("Welcome\nEveryone",
+                      style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold),
+
+                  )
+                ],
+              ),)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+}
